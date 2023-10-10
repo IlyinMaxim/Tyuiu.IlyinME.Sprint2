1f40c5aa97cf237c2b661b0b270aa341e3c886cd
Tyuiu.IlyinME@outlook.com <maxchance05@mail.ru>
2023-10-10 17:54:01 +0500

    Task4

---
+++ Tyuiu.IlyinME.Sprint2.Task4.V8.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.IlyinME.Sprint2.Task4.V8.Lib;
+
+namespace Tyuiu.IlyinME.Sprint2.Task4.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 6;
+            double y = -4;
+            double res = ds.Calculate(x, y);
+            double wait = 15712.007;
+            Assert.AreEqual(wait, res); 
+        }
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 5;
+            double y = 3;
+            double res = ds.Calculate(x, y);
+            double wait = 55.333;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.IlyinME.Sprint2.Task4.V8/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.IlyinME.Sprint2.Task4.V8.Lib;
+
+namespace Tyuiu.IlyinME.Sprint2.Task4.V8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Ильин М. Е. | АСОиУБ-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Тернарный оператор                                                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #8                                                              *");
+            Console.WriteLine("* Выполнил: Ильин Максим Евгеньевич | АСОиУБ-23-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение                *");
+            Console.WriteLine("* с использованием тернарного оператора, шде пользователь вводит значение *");
+            Console.WriteLine("* переменных x, y с клавиатуры, если x * 2 > y + 15, то                   *");
+            Console.WriteLine("* z = (5 + Sqrt(y + 5) / x ^ 3) ^ x иначе                                 *");
+            Console.WriteLine("* 2x ^ 2 + 2x - (4 + 6 / y ^ 2)                                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение переменной X:");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной Y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double res = ds.Calculate(x, y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Значение функции = " + res);
+
+            Console.ReadKey();
+        }
+    }
+}
